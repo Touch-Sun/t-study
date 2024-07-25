5c68d374fc52d1bb376c2020dd029d41ca0fe06a
lee.li <lee.li@qq.com>
2024-07-25 18:06:28 +0800

    init project

---
+++ src/main/java/io/github/touchsun/tstudy/TStudyApplication.java
@@ -0,0 +1,18 @@
+package io.github.touchsun.tstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * t-study
+ *
+ * @author lee
+ * @since 2024/7/25 17:13
+ */
+@SpringBootApplication
+public class TStudyApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TStudyApplication.class, args);
+    }
+}
