ce20bb1091e3c12d100f03cde0e7d6ecaa0a2fa0
lee.li <lee.li@qq.com>
2024-07-28 14:23:29 +0800

    feat: 增加前端

--- page/src/main.js
+++ page/src/main.js
@@ -1,12 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+// 引入 Bootstrap 和 Bootstrap-Vue 样式
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// 引入全局样式
+import './styles/global.css';
+
+// 引入 Bootstrap-Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+Vue.config.productionTip = false;
+
+// 安装 BootstrapVue
+Vue.use(BootstrapVue);
+// 可选的安装 BootstrapVue icon 插件
+Vue.use(IconsPlugin);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
